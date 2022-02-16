667a8d046f8b812b5cc45a9977d2947ad2694b7e
paulosoujava <paulosoujava@gmail.com>
2022-02-16 13:57:49 -0300

    initial commit

---
+++ src/main/kotlin/StopWatchDisplay.kt
@@ -0,0 +1,51 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun StopWatchDisplay(
+    formattedTime: String,
+    onStartClick: () -> Unit,
+    onPauseClick: () -> Unit,
+    onResetClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = formattedTime,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = Color.Black
+        )
+        Spacer(Modifier.width(16.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ){
+           Button(onStartClick){
+               Text(text ="PLAY")
+           }
+            Spacer(Modifier.width(16.dp))
+            Button(onPauseClick){
+                Text(text ="PAUSE")
+            }
+            Spacer(Modifier.width(16.dp))
+            Button(onResetClick){
+                Text(text ="RESET")
+            }
+        }
+    }
+}
